08e518b86e3d669763ea5f2f6a4ccc55b9cf080d
teodordediu2017 <teodor.dediu.66@lacetania.cat>
2018-02-07 20:06:10 +0100

    cumunicacio
    asdfger8potauwsdf8i7q0aiy vq87wbgef87ogof87afog87cawgrf8pb7q bp9q 4lib8 qgao8r

---
+++ Cuminuci__2/Cuminuci__2.ino
@@ -0,0 +1,12 @@
+void setup() {
+Serial.begin(9600);  // obre el port sèrie configurant la velocitat a 9600bps//  
+Serial.println ("Escull el numero de l'operació que vols realitzar?");
+Serial.println ("1. Comprovar numero de tarjeta de credit");
+Serial.println ("2. Comprova numero de compte bancari ");
+Serial.println ("3. Busca un digit perdut de tarjeta de credit ");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
